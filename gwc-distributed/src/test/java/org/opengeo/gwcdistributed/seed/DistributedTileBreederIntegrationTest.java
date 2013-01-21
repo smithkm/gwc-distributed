3e24535df21b1a67c2c6a21c43b567d3d5ebeeea
Kevin Smith <ksmith@opengeo.org>
2013-01-21 12:58:26 -0800

    Distributed seed job termination

--- gwc-distributed/src/test/java/org/opengeo/gwcdistributed/seed/DistributedTileBreederIntegrationTest.java
+++ gwc-distributed/src/test/java/org/opengeo/gwcdistributed/seed/DistributedTileBreederIntegrationTest.java
@@ -1,6 +1,8 @@
 package org.opengeo.gwcdistributed.seed;
 
 import static org.junit.Assert.*;
+import static org.easymock.classextension.EasyMock.*;
+import static org.hamcrest.Matchers.*;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -22,8 +24,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.hazelcast.core.Hazelcast;
-
-import static org.easymock.classextension.EasyMock.*;
 
 public class DistributedTileBreederIntegrationTest {
 	static final String CONTEXT_PATH = "classpath:/org/opengeo/gwcdistributed/seed/distributedTileBreederTest-context.xml";
@@ -168,6 +168,93 @@ public class DistributedTileBreederIntegrationTest {
 		for(SeedTask task: tasks2){
 			verify(task);
 		}
+	}
+	
+	@Test
+	public void testTerminateJob() throws Exception {
+		
+		Collection<SeedTask> tasks1 = Collections.singleton(createMock(SeedTask.class));
+		long taskId=0;
+		for(SeedTask task: tasks1){
+			expect(task.getState()).andStubReturn(STATE.READY);
+			expect(task.getTaskId()).andStubReturn(taskId++);
+			AbstractJobTest.expectDoActionInternal(task);
+			AbstractJobTest.expectDispose(task);
+			//replay(task);
+		}
+		breeder1.seedIt=tasks1.iterator();
+		
+		Collection<SeedTask> tasks2 = Collections.singleton(createMock(SeedTask.class));
+		for(SeedTask task: tasks2){
+			expect(task.getState()).andStubReturn(STATE.READY);
+			expect(task.getTaskId()).andStubReturn(taskId++);
+			AbstractJobTest.expectDoActionInternal(task);
+			AbstractJobTest.expectDispose(task);
+			//replay(task);
+		}
+		breeder2.seedIt=tasks2.iterator();
+		
+		@SuppressWarnings("deprecation")
+		TileRange tr = new TileRange("testLayer", null, 0, 0, null, null, (String) null);
+		
+		TileRangeIterator trItr = createMock(TileRangeIterator.class); {
+			expect(trItr.getTileRange()).andStubReturn(tr);
+		} replay(trItr);
+		
+		TileLayer tl = createMock(TileLayer.class); {
+			expect(tl.getName()).andStubReturn("testLayer");
+			expect(tl.getMetaTilingFactors()).andStubReturn(new int[]{1,1});
+		} replay(tl);
+		
+		TileLayerDispatcher tld = createMock(TileLayerDispatcher.class); {
+			expect(tld.getTileLayer("testLayer")).andStubReturn(tl);
+		} replay(tld);
+		
+		breeder1.setTileLayerDispatcher(tld);
+		breeder2.setTileLayerDispatcher(tld);
+		
+		Job job = breeder1.createJob(tr, GWCTask.TYPE.SEED, 1, false);
+		final long id = job.getId();
+		
+		System.out.println("Waiting to give the job a chance to propagate");
+		Thread.sleep(2000);
+		
+		
+		System.out.println("Dispatching Job");
+		breeder1.dispatchJob(job);
+
+		System.out.println("Waiting to give the dispatch a chance to propagate");
+		Thread.sleep(2000);
+		
+		// Check that the right calls were made for dispatch, then expect 
+		// the calls for termination
+		for(SeedTask task: tasks1){
+			verify(task);
+			
+			reset(task);
+			task.terminateNicely();
+			expect(task.getState()).andStubReturn(STATE.RUNNING);
+			replay(task);
+		}
+		for(SeedTask task: tasks2){
+			verify(task);
+			
+			reset(task);
+			task.terminateNicely();
+			expect(task.getState()).andStubReturn(STATE.RUNNING);
+			replay(task);
+		}
+		// Terminate
+		job.terminate();
+		
+		// Verify termination
+		for(SeedTask task: tasks1){
+			verify(task);
+		}
+		for(SeedTask task: tasks2){
+			verify(task);
+		}
+		
 	}
 
 
